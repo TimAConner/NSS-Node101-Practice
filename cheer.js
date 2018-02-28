2d7a237962c8b787fce53e463eba88a811f8c339
Tim A. Conner <timaconner1@gmail.com>
2018-02-28 15:05:44 -0600

    Add first exercise.

---
+++ cheer.js
@@ -0,0 +1,28 @@
+const [,,...cheerTextArray] = process.argv;
+const cheerText = cheerTextArray.reduce((ac, cv) => ac.toUpperCase() + cv.toUpperCase()); 
+
+const letterInterval = 100;
+const vowelSounds = ["O", "H", "E", "N"]
+
+const printDelayedText = (delay, text) => {
+    setTimeout(() => {
+        process.stdout.write(`${text}\n`);
+    }, delay);
+}
+
+let letterIndex = 0;
+
+
+for(const letter of [...cheerText]){
+    letterIndex++;
+
+    delayTime = letterInterval * letterIndex;
+
+    printDelayedText(delayTime, `Give me ${vowelSounds.includes(letter) ? 'an' : 'a'} ${letter}!`)
+
+    const {length: finalIndex} = cheerText;
+    if(letterIndex === finalIndex) {
+        printDelayedText(delayTime+(letterInterval),  "What does that spell?");
+        printDelayedText(delayTime+(letterInterval*2), `${cheerText}!`);
+    }
+}
